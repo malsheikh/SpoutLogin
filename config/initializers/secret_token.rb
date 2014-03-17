eda4a9d137032b5f1f548c4322e85c2be64d2c37
MattySheikh <malsheikh@findthebest.com>
2014-03-16 22:18:45 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpoutLogin::Application.config.secret_key_base = '2c2bd60fc35914b6d034496bedac665700b9d11dce6810f2e2ae9e2ea24b3afc8ad68b5ac397f9f73069f7d7f60ba43746038ce2643745d6bbc552fdea78e2a5'
